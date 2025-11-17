1512956fb31e7c2a1e52737780487448b1029b55
JoshSandoy <leeken851@gmail.com>
2025-11-17 08:28:00 +0800

    first commit

---
+++ cs.js
@@ -0,0 +1,22 @@
+function legalAge(grade){
+
+if(grade >= 94.5){
+    console.log("1");
+} else if (grade >=91.5){
+    console.log("1.25"); 
+
+} else if (grade >=89.5){
+    console.log("1.25");  
+
+} else if (grade >=86.5){
+    console.log("1.75"); 
+    
+} else if (grade >=84.5){
+    console.log("2");  
+
+}else{
+    console.log("you are not an adult");
+
+}
+}
+computeGrade(85);
---
+++ math.js
@@ -0,0 +1,9 @@
+function sum(num1,num2){
+    sum = num1 + num2;
+    console.log(sum);
+}
+
+sum(2,3);
+
+function diff(num1,num2){
+}
